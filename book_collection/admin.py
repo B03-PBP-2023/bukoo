e277be2575532ccd84672624b8db8b6b6dd47079
ilhamelhamdi <ilhamabdillah123@gmail.com>
2023-10-28 13:52:13 +0700

    fix: integrate book_collection with user_profile

--- book_collection/admin.py
+++ book_collection/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from book_collection.models import Book, Genre, MockAuthorProfile
+from book_collection.models import Book, Genre
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Genre)
-admin.site.register(MockAuthorProfile)
