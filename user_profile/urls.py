e42652cc5c616fd7d0b4e3d7439d3500d96210f0
FBK15 <asadilhaqklem@gmail.com>
2023-10-28 17:19:54 +0700

    update views dan models

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'user_profile'
 
 urlpatterns = [
-    
+    path('edit-profile', views.edit_profile, name = 'edit-profile'),
+    path('user-profile', views.profile_data, name = 'user-profile'),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from user_profile.models import Profile, Bookmark
 from user_profile.forms import ProfileForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-from book_collection import Book
+from book_collection.models import Book
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def profile_data(request):
     bookmarked_book = Bookmark.objects.filter(user=request.user)
     user_data = Profile.objects.filter(user=request.user)
@@ -20,17 +22,30 @@ def profile_data(request):
 
     return render(request, 'profile.html', data)
 
+@login_required
 def edit_profile(request):
     profile = Profile.objects.get(pk=id)
     form = ProfileForm(request.POST or None, instance=profile)
 
-    if form.is_valid() and request.method == 'POST':
-        form.save()
-        return HttpResponseRedirect(reverse('user_profile:profile'))
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('profile:user_profile'))
     
     return render(request, 'edit_profile.html', {'form': form})
 
+@login_required
 def bookmarking_books(request, book_id):
-    books = Book
+    books = Book.objects.get(pk=book_id)
+    Bookmark.objects.create(user=request.user, book=books)
+
+    return HttpResponse("CREATED", status=201)
+
+@login_required
+def delet_bookmark(request, book_id):
+    bookmarks = Bookmark.objects.get(pk=book_id)
+    bookmarks.delete()
+
+    return HttpResponseRedirect(reverse('profile:user_profile'))
 
 
