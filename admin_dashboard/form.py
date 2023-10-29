8f1688d839474da836c593c3ec0068dcefcfbed8
fayyazheva <azhevaf@gmail.com>
2023-10-29 22:54:53 +0700

    add form checking for admin dashboard

--- admin_dashboard/form.py
+++ admin_dashboard/form.py
@@ -1,16 +1,23 @@
+# from django import forms
+# from admin_dashboard.models import BookData
+
+# class BookDataForm(forms.ModelForm):
+#     class Meta:
+#         model = BookData
+#         fields = [
+#             'title',
+#             'description',
+#             'genres',
+#             'publisher',
+#             'language',
+#             'isbn',
+#             'num_pages',
+#             'publish_date',
+#         ]
 from django import forms
-from admin_dashboard.models import BookData
+from .models import AdminBook
 
 class BookDataForm(forms.ModelForm):
     class Meta:
-        model = BookData
-        fields = [
-            'title',
-            'description',
-            'genres',
-            'publisher',
-            'language',
-            'isbn',
-            'num_pages',
-            'publish_date',
-        ]
+        model = AdminBook
+        fields = '__all__'
