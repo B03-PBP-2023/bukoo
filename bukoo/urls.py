762d613eb290ae5ef17d68bb42297910edf41fee
ilhamelhamdi <ilhamabdillah123@gmail.com>
2023-11-30 12:23:34 +0700

    fix: static files azure deployment

--- bukoo/urls.py
+++ bukoo/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf import settings
 
 
 urlpatterns = [  
@@ -31,5 +31,5 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
