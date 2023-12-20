25227f1dfec3566eb5be311098884302b91b0297
ilhamelhamdi <ilhamabdillah123@gmail.com>
2023-12-20 20:08:31 +0700

    Refactor Profile model and add natural_key method

--- user_profile/models.py
+++ user_profile/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from auth.models import User
 from book_collection.models import Book
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=255, null=True, blank=True)
     about_user = models.TextField(null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
@@ -11,15 +13,26 @@ class Profile(models.Model):
     prefered_genre = models.CharField(max_length=255, null=True, blank=True)
     profile_picture = models.URLField(null=True, blank=True)
 
+    def __str__(self) -> str:
+        return f"[{self.pk}] {self.name}"
+
     def natural_key(self):
         name = self.name
+        about_user = self.about_user
+        date_of_birth = self.date_of_birth
+        gender = self.gender
+        preferred_genre = self.prefered_genre
+        profile_picture = self.profile_picture
+
         if self.user:
             self.user.username
-        return {'id':self.pk, 'name':name}
+        return {'id': self.pk, 'name': name, 'about_user': about_user, 'date_of_birth': date_of_birth, 'gender': gender, 'prefered_genre': preferred_genre, 'profile_picture': profile_picture}
+
 
 class Bookmark(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Book_by_author(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
