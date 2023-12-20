abd4a27f0059ddafd37b8329a40697e2e10cfa58
FBK15 <asadilhaqklem@gmail.com>
2023-12-20 12:48:43 +0700

    update routing serializers

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('api/profile/bookmark-book/<int:book_id>/', views.bookmarking_books, name = 'bookmark-book'),
     path('api/profile/delete-bookmark/<int:book_id>/', views.delete_bookmark, name = 'delete-bookmark'),
     path('api/profile/get-bookmark-status/<int:book_id>/', views.get_bookmark_status, name = 'get-bookmark-status'),
-    path('api/profile/json/', views.show_json, name = 'show_profile-json'),
-    path('api/profile/xml/', views.show_xml, name = 'show_profile_xml'),
-    path('api/profile/xml/<int:id>/', views.show_xml_id, name = 'profile_xml_id'),
-    path('api/profile/json/<int:id>/', views.show_json_id, name = 'profile_json_id'),
+    path('profile/json/', views.show_json, name = 'show_profile-json'),
+    path('profile/xml/', views.show_xml, name = 'show_profile_xml'),
+    path('profile/xml/<int:id>/', views.show_xml_id, name = 'profile_xml_id'),
+    path('profile/json/<int:id>/', views.show_json_id, name = 'profile_json_id'),
 ]
